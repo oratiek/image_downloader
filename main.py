c50e0f46da702f0e215048ed0715d8e9f1620286
oratiek <oratiek522@gmail.com>
2020-06-11 13:14:53 +0900

    main script

---
+++ main.py
@@ -0,0 +1,66 @@
+import sys
+import requests
+from bs4 import BeautifulSoup
+import os
+
+
+"""
+枚数の計算処理がおぞくなっているが大方OK
+"""
+
+class Downloader:
+    def __init__(self,keyword,dirname,cnt):
+        if os.path.exists(dirname):
+            pass
+            print("Specified Directory ")
+        else:
+            print("Create new file")
+            os.mkdir(dirname)
+        self.dirname = dirname
+        self.keyword = keyword
+        self.cnt = cnt
+
+    def download(self, url, filepath):
+        binary = requests.get(url, stream=True)
+        if binary.status_code == 200:
+            with open(filepath,"wb") as f:
+                f.write(binary.content)
+                print(url,"\n {} download completed".format(filepath))
+        else:
+            print(url, "\n {} donwload failed".format(filepath))
+
+    def get_urls(self, keyword, cnt):
+        if keyword == "":
+            print("No keyword specified")
+            sys.exit(1)
+        if cnt > 60:
+            pages = int(cnt/60) + 1
+            last_page_cnt = cnt - 60*pages
+        else:
+            pages = 1
+        # http requests
+        img_urls = []
+        for page in range(pages):
+            if page == pages - 1: # in the last page
+                url = "https://search.yahoo.co.jp/image/search?p={}&n={}&b=11".format(keyword, last_page_cnt, page)
+            else:
+                url = "https://search.yahoo.co.jp/image/search?p={}&n={}&b=11".format(keyword, cnt, page)
+            r = requests.get(url)
+            soup = BeautifulSoup(r.text, features="html.parser")
+            img_tags = soup.find_all("img")
+            img_urls_tmp = [e.get("src") for e in img_tags]
+            for url_tmp in img_urls_tmp:
+                img_urls.append(url_tmp)
+        return img_urls
+
+
+    def main(self):
+        urls = self.get_urls(self.keyword, self.cnt)
+        filenum = 0
+        for url in urls:
+            filename = self.dirname + "/" + str(filenum) + ".jpg"
+            self.download(url, filename)
+            filenum += 1
+
+downloader = Downloader("ボール","ball",100)
+downloader.main()
